4b70817f8f629f9c23d4906023c7bd7741d35cf3
binghuan <binghuanlin@gmail.com>
2016-10-09 21:50:38 +0800

    Change TVList.

--- js/tvList.js
+++ js/tvList.js
@@ -7,33 +7,13 @@ var tvList = [{
     "title1": "CTS Taiwan News Live HD",
     "title2": "華視新聞HD直播"
 }, {
-    "id": "hgIfZz8STLk",
+    "id": "psBnG0ZvGmU",
     "title1": "CTITV Taiwan News HD Live",
     "title2": "CTI中天新聞台24小時新聞HD1080P直播"
-}, {
-    "id": "jMN4cxyhJjk",
-    "title1": " Taiwan EBC 24h live news",
-    "title2": "EBC 東森新聞 51 頻道 24 小時線上直播"
 }, {
     "id": "XxJKnDLYZz4",
     "title1": "Taiwan Formosa live news HD",
     "title2": "台灣民視新聞HD直播"
-}, {
-    "id": "z9L9T2tetMQ",
-    "title1": "Chinese Public TV",
-    "title2": "公共電視 網路直播"
-}, {
-    "id": "v9j-jFkcNf4",
-    "title1": "udn tv",
-    "title2": "udn tv"
-}, {
-    "id": "7jeteD5lcYE",
-    "title1": "CCTV News",
-    "title2": "CCTV News"
-}, {
-    "id": "ysbHhsXDxg8",
-    "title1": "東網直播",
-    "title2": "東網直播"
 }, {
     "id": "Giw92W9xkys",
     "title1": "EBC 股動錢潮 LIVE",
@@ -47,11 +27,12 @@ var tvList = [{
     "title1": "大愛一台HD Live直播",
     "title2": "大愛一台HD Live直播"
 }, {
-    "id": "PfaVDgd17Io",
-    "title1": "Euronews LIVE",
-    "title2": "Euronews LIVE"
-}, {
     "id": "y60wDzZt8yg",
     "title1": "Skye News Live",
     "title2": "Skye News Live"
-}];
+},{
+"id": "jMN4cxyhJjk",
+"title1": " Taiwan EBC 24h live news",
+"title2": "EBC 東森新聞 51 頻道 24 小時線上直播"
+}
+];
