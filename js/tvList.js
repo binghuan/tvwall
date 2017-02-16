63a2306e35e9da689560425ef3d52f2474d11c17
binghuan <binghuanlin@gmail.com>
2017-02-16 15:49:14 +0800

    Change channels.

--- js/tvList.js
+++ js/tvList.js
@@ -1,38 +1,38 @@
-var tvList = [{
-    "id": "yk2CUjbyyQY",
-    "title1": "TAIWAN TTV NEWS HD (Live)",
-    "title2": "台視新聞台HD直播"
-}, {
-    "id": "g9uJqP0hT_I",
-    "title1": "CTS Taiwan News Live HD",
-    "title2": "華視新聞HD直播"
-}, {
-    "id": "psBnG0ZvGmU",
-    "title1": "CTITV Taiwan News HD Live",
-    "title2": "CTI中天新聞台24小時新聞HD1080P直播"
-}, {
-    "id": "XxJKnDLYZz4",
-    "title1": "Taiwan Formosa live news HD",
-    "title2": "台灣民視新聞HD直播"
-}, {
-    "id": "Giw92W9xkys",
-    "title1": "EBC 股動錢潮 LIVE",
-    "title2": "EBC 股動錢潮 LIVE"
-}, {
-    "id": "r_urFyI0uvE",
-    "title1": "57彩券王",
-    "title2": "57彩券王"
-}, {
-    "id": "ESKjSwcswBM",
-    "title1": "大愛一台HD Live直播",
-    "title2": "大愛一台HD Live直播"
-}, {
-    "id": "y60wDzZt8yg",
-    "title1": "Skye News Live",
-    "title2": "Skye News Live"
-},{
-"id": "jMN4cxyhJjk",
-"title1": " Taiwan EBC 24h live news",
-"title2": "EBC 東森新聞 51 頻道 24 小時線上直播"
-}
+var tvList = [
+    
+    {
+        "id": "CUKpfr9tWBc",
+        "title1": "Taiwan EBC 24h live finance news",
+        "title2": "EBC 東森財經新聞 57 頻道 24 小時線上直播"
+    },
+    {
+        "id": "TgGyBF-7w8M",
+        "title1": "SET News Live",
+        "title2": "三立新聞直播"
+    },
+    {
+        "id": "yk2CUjbyyQY",
+        "title1": "TAIWAN TTV NEWS HD (Live)",
+        "title2": "台視新聞台HD直播"
+    }, {
+        "id": "VsvZqiB2y1o",
+        "title1": "CTITV Taiwan News HD Live",
+        "title2": "CTI中天新聞24小時HD新聞直播"
+    }, {
+        "id": "XxJKnDLYZz4",
+        "title1": "Taiwan Formosa live news HD",
+        "title2": "台灣民視新聞HD直播"
+    }, {
+        "id": "ESKjSwcswBM",
+        "title1": "大愛一台HD Live直播",
+        "title2": "大愛一台HD Live直播"
+    }, {
+        "id": "y60wDzZt8yg",
+        "title1": "Skye News Live",
+        "title2": "Skye News Live"
+    }, {
+        "id": "yzE3bRtXIrI",
+        "title1": " Taiwan EBC 24h live news",
+        "title2": "EBC 東森新聞 51 頻道 24 小時線上直播"
+    }
 ];
