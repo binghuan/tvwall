62c1b5f5714286ace0787da1d1eb4750c48d784b
BH_Lin <binghuanlin@gmail.com>
2017-12-31 22:48:56 +0800

    update tvList.

--- js/tvList.js
+++ js/tvList.js
@@ -1,38 +1,18 @@
 var tvList = [{
-    "id": "yk2CUjbyyQY",
-    "title1": "TAIWAN TTV NEWS HD (Live)",
-    "title2": "台視新聞台HD直播"
+    "id": "miI6KnXkR64",
+    "title1": "2018飛躍桃園直達美好跨年晚會",
+    "title2": "2018飛躍桃園直達美好跨年晚會"
 }, {
-    "id": "g9uJqP0hT_I",
-    "title1": "CTS Taiwan News Live HD",
-    "title2": "華視新聞HD直播"
+    "id": "u6wyRhL2ZL4",
+    "title1": "2018臺北最HIGH新年城】市政府廣場跨年晚會LIVE直播",
+    "title2": "2018臺北最HIGH新年城】市政府廣場跨年晚會LIVE直播"
 }, {
-    "id": "psBnG0ZvGmU",
-    "title1": "CTITV Taiwan News HD Live",
-    "title2": "CTI中天新聞台24小時新聞HD1080P直播"
+    "id": "c0fdh1CoCUc",
+    "title1": "2018台南心時代跨年晚會【直播】",
+    "title2": "2018台南心時代跨年晚會【直播】"
 }, {
-    "id": "XxJKnDLYZz4",
-    "title1": "Taiwan Formosa live news HD",
-    "title2": "台灣民視新聞HD直播"
-}, {
-    "id": "Giw92W9xkys",
-    "title1": "EBC 股動錢潮 LIVE",
-    "title2": "EBC 股動錢潮 LIVE"
-}, {
-    "id": "r_urFyI0uvE",
-    "title1": "57彩券王",
-    "title2": "57彩券王"
-}, {
-    "id": "ESKjSwcswBM",
-    "title1": "大愛一台HD Live直播",
-    "title2": "大愛一台HD Live直播"
-}, {
-    "id": "y60wDzZt8yg",
-    "title1": "Skye News Live",
-    "title2": "Skye News Live"
-},{
-"id": "jMN4cxyhJjk",
-"title1": " Taiwan EBC 24h live news",
-"title2": "EBC 東森新聞 51 頻道 24 小時線上直播"
+    "id": "VNU1MR8qNtk",
+    "title1": "2018愛Sharing高雄夢時代跨年派對【直播】",
+    "title2": "2018愛Sharing高雄夢時代跨年派對【直播】"
 }
 ];
