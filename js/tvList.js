2d6636ccf61b5afbe16444ff6742423228f4b1aa
binghuan <binghuanlin@gmail.com>
2018-02-16 16:53:08 +0800

    update tvlist.

--- js/tvList.js
+++ js/tvList.js
@@ -1,18 +1,42 @@
-var tvList = [{
-    "id": "miI6KnXkR64",
-    "title1": "2018飛躍桃園直達美好跨年晚會",
-    "title2": "2018飛躍桃園直達美好跨年晚會"
-}, {
-    "id": "u6wyRhL2ZL4",
-    "title1": "2018臺北最HIGH新年城】市政府廣場跨年晚會LIVE直播",
-    "title2": "2018臺北最HIGH新年城】市政府廣場跨年晚會LIVE直播"
-}, {
-    "id": "c0fdh1CoCUc",
-    "title1": "2018台南心時代跨年晚會【直播】",
-    "title2": "2018台南心時代跨年晚會【直播】"
-}, {
-    "id": "VNU1MR8qNtk",
-    "title1": "2018愛Sharing高雄夢時代跨年派對【直播】",
-    "title2": "2018愛Sharing高雄夢時代跨年派對【直播】"
-}
+var tvList = [
+    {
+        "id": "zjGR32QyTkQ",
+        "title1": "公共電視 網路直播 PTS Live",
+        "title2": "公共電視 網路直播 PTS Live"
+    }
+    , {
+        "id": "XxJKnDLYZz4",
+        "title1": "台灣民視新聞HD直播",
+        "title2": "Taiwan Formosa live news HD"
+    }
+    , {
+        "id": "WAGIkirjfCw",
+        "title1": "華視新聞HD直播",
+        "title2": "CTS Taiwan News Live HD"
+    }
+    , {
+        "id": "Biolfit9JkE",
+        "title1": "EBC 東森新聞 51 頻道 24 小時線上直播",
+        "title2": "Taiwan EBC 24h live news"
+    }
+    , {
+        "id": "4ZVUmEUFwaY",
+        "title1": "三立LIVE新聞HD直播",
+        "title2": "SET Live NEWS"
+    }
+    , {
+        "id": "KXeFVTsFrCs",
+        "title1": "中視新聞台 LIVE直播",
+        "title2": "Taiwan CTV news HD Live"
+    }
+    , {
+        "id": "wUPPkSANpyo",
+        "title1": "CTI中天新聞24小時HD新聞直播",
+        "title2": "CTITV Taiwan News HD Live"
+    }
+    , {
+        "id": "ESKjSwcswBM",
+        "title1": "大愛一臺HD Live 直播",
+        "title2": "大愛一臺HD Live 直播"
+    }
 ];
